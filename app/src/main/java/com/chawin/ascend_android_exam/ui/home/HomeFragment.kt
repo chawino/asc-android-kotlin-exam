78061ba4dc95bce203e23706db59a3a048751e7c
W!N <windzie@gmail.com>
2021-07-01 21:24:42 +0700

    Initial HomeFragment

---
+++ app/src/main/java/com/chawin/ascend_android_exam/ui/home/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.chawin.ascend_android_exam.ui.home
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleObserver
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment(), LifecycleObserver {
+}
